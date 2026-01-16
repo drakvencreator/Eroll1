89af945bf8b5c5707cfc4def4541da1e944e3f99
drakvencreator <andokosova@gmail.com>
2026-01-17 00:34:04 +0100

    Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -59,7 +59,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentView, onNavigate }) => {
                     ---------------------------------------------------------------------------------
                  */
                  <img 
-                  src="https://ibb.co/MDbgp8LQ" 
+                  src="https://i.ibb.co/N6XFp1zb/logo.png" 
                   alt="Auto Japan Erolli" 
                   className="h-20 md:h-28 w-auto object-contain relative z-10 drop-shadow-[0_0_15px_rgba(0,0,0,1)] transition-transform duration-300 group-hover:scale-110 group-hover:-rotate-2 hover-rumble"
                   onError={() => setImgError(true)}
@@ -136,7 +136,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentView, onNavigate }) => {
             {/* Logo in Mobile Menu as well */}
             <div className="flex justify-center mb-6 pb-4 border-b border-gray-900">
                {!imgError ? (
-                 <img src="https://ibb.co/MDbgp8LQ" alt="Logo" className="h-24 w-auto animate-pulse" />
+                 <img src="https://i.ibb.co/N6XFp1zb/logo.png" alt="Logo" className="h-24 w-auto animate-pulse" />
                ) : (
                  <span className="font-aggressive text-3xl text-white">AUTO JAPAN <span className="text-red-600">EROLLI</span></span>
                )}
